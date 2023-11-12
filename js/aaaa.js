ca387e62989b92a2555f4c7682f53cf453475888
Dziiugas <145454289+Dziiugas@users.noreply.github.com>
2023-11-12 21:02:15 +0200

    Add files via upload

--- js/aaaa.js
+++ js/aaaa.js
@@ -1,10 +1,37 @@
-document.querySelector('form').addEventListener('button',(e)=>{
-    e.preventDefault();
-        function shuffleGallery(){
-            const images = Array.from(gallery.children);
-            images.sort(() => Math.random() - 0.5);
-            images.forEach((img)=> gallery.appendChild(img));
-        
-    }   
-        return shuffleGallery 
-})
+document.addEventListener("DOMContentLoaded", function () {
+    const imagesli = document.querySelector(".gallery");
+    const revealButton = document.querySelector(".rodyti");
+    revealButton.addEventListener("click", function () {
+      revealButton.style.display = "show";
+      mixButton.style.display = "block";
+      createImages(imagesli);
+    });
+  
+    const mixButton = document.querySelector(".mix");
+    mixButton.style.display = "show";
+    mixButton.addEventListener("click", function () {
+      mix(imagesli);
+    });
+  });
+  
+  function createImages(container) {
+    container.innerHTML = "";
+    for (let i = 1; i <= 10; i++) {
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      img.src = `img/image${i}.jpg`;
+      img.alt = `Image ${i}`;
+      img.addEventListener("dblclick", function () {
+        img.src = "img/vienas.jpg";
+      });
+  
+      li.appendChild(img);
+      container.appendChild(li);
+    }
+  }
+  
+  function mix(container) {
+    for (let i = container.children.length; i >= 0; i--) {
+      container.appendChild(container.children[(Math.random() * i) | 0]);
+    }
+  }
